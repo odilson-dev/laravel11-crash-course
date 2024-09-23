546305d02cdcfe351ff6b774e6d8313c863364d0
Odilson Woodly Italis <italisodilsonwoodly@hotmail.com>
2024-09-23 10:34:48 -0400

    Create Routes for Note

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\NoteController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [WelcomeController::class,'welcome'])->name('Welcome');
+Route::resource('note', NoteController::class);
